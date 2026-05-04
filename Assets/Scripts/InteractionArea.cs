1bca61250fcb96abfaa0f1a5f5a267345f453c3d
matid2710 <mdobisky@gmail.com>
2026-05-04 10:08:56 -0300

    Update InteractionArea.cs

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision col)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hubo colision");
+
     }
 }
